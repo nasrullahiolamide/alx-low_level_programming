b05a315436b2c11223c72113d9713c05bb481a8a
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-03 20:40:32 +0100

    Print numbers

--- 0x01-variables_if_else_while/alpha.c
+++ 0x01-variables_if_else_while/alpha.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
 int main() {
-    char c;
-    for (c = 'a'; c <= 'z'; ++c)
-    {
-        if (c == 'q' || c == 'e')
-            continue;
-        putchar(c);
-    }
-        
+    int i;
+    for (i = 0; i < 10 ; i++)
     
+        printf("%d", i);
+    
+        
     // for (c = 'a'; c <= 'z'; ++c)
         // putchar(toupper(c));
     
