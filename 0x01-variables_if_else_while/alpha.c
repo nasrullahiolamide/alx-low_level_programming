91e962a5560acc915cfb500fa3adc4299157922e
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-03 20:12:32 +0100

    Print alphabets

--- 0x01-variables_if_else_while/alpha.c
+++ 0x01-variables_if_else_while/alpha.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 int main() {
     char c;
     for (c = 'a'; c <= 'z'; ++c)
+    {
+        if (c == 'q' || c == 'e')
+            continue;
         putchar(c);
+    }
+        
+    
+    // for (c = 'a'; c <= 'z'; ++c)
+        // putchar(toupper(c));
+    
     putchar('\n');
     return 0;
 }
