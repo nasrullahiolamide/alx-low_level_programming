61d0df41d76223ed0c34cb6dab1adc7a3f3a49ce
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-07 15:54:02 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point of the program
+ * description - To determine whether a number is negative or positive
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
