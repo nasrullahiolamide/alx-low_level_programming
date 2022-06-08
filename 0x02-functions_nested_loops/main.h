59c12b451c8620db655541884dc8a1c4a366707f
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-08 08:05:59 +0100

    is alpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,11 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 
+/**
+ * is_alpha - check if a character is an alphabet
+ * @c : character to check 
+ * Return: return 1 if success and 0 is not 
+ */
+int _isalpha(int c);
+
 #endif
