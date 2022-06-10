0a568a71166fec75bb12cb6a78bd31a9d06016fe
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-10 05:17:31 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: an int
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
+
+
+
+#endif /* MAIN_H */
