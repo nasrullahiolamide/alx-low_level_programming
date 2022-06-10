1a98374c32684101d349a37c44cfffec047d20ae
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-10 05:20:33 +0100

    is digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,6 +17,13 @@ int _putchar(char c);
 
 int _isupper(int c);
 
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c);
 
 
 #endif /* MAIN_H */
