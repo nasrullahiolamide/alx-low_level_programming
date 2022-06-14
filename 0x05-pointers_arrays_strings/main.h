a9dc3829bb07df65b69c1062159a34b53062f764
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-14 23:05:08 +0100

    reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,15 +9,43 @@
 
 int _putchar(char c);
 
+/**
+* reset_to_98 - reset the value of a variable to 98
+* @n: pointer to the variable to reset
+* Return: void
+*/
 
 void reset_to_98(int *n);
 
-
+/**
+ * swap_int - swap the values of two variables
+ * @a: pointer to the first variable
+ * @b: pointer to the second variable
+ * Return: void
+ */
 void swap_int(int *a, int *b);
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be measured
+ * Return: length of string which is an integer
+ **/
 int _strlen(char *s);
 
+/**
+ * _puts - Prints out a string into std output
+ * @str: string to read
+ * Return : null
+ */
 void _puts(char *str);
+
+/**
+ * print_rev - Prints out a string into std output
+ * @s: string to read
+ * Return : null
+ */
+void print_rev(char *s);
+
 
 
 #endif /* MAIN_H */
