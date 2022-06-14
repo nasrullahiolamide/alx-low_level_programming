dc9aec70b149dcb79d95c5da197264a96b1a249e
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-14 14:20:52 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: an int
+ */
+
+int _putchar(char c);
+
+
+void reset_to_98(int *n);
+
+
+
+#endif /* MAIN_H */
