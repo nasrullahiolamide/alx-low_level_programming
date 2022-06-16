34ba88c576ced8eeb2f2291caebf3e15670284ed
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-16 23:00:33 +0100

    memset

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _memset - fill n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* MAIN_H */
