a46f4b4ddae07238bef71aac62c6bc00e2f87be5
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-20 20:18:31 +0100

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,16 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	int size = n; /* only accept positive sizes */
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			s[i] = b;
+	}
+
 	return (s);
 }
