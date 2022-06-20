b2834ce0289a7d00325ad0061b00151552f3a925
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-20 23:24:06 +0100

    add

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -44,6 +44,14 @@ char *_strchr(char *s, char c);
  */
 unsigned int _strspn(char *s, char *accept);
 
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string to search
+ * @accept: target matches
+ * Return: pointer to index of string at first occurence
+ */
+
+char *_strpbrk(char *s, char *accept);
 
 
 #endif /* MAIN_H */
