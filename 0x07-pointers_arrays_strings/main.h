abf8d4c85a3b684c9436c9249f296633560c67f5
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-06-20 23:35:10 +0100

    diagsum

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -58,12 +58,21 @@ char *_strpbrk(char *s, char *accept);
  * @needle: target substring to search for
  * Return: pointer to index of string at first occurence of whole substring
  */
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle);
 
 /**
  * print_chessboard - print chessboard given set 2D array
  * @a: 2D array
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8]);
+
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ * @a: matrix
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size);
+
+
 
 #endif /* MAIN_H */
