f9f86afd464b4740f798ccdff3f1eb9acf1954da
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-04 23:42:35 +0100

    print dog credntials

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	(d->name == NULL) ? printf("Name:(nil)")
+	(d->name == NULL) ? printf("Name: (nil)")
 		: printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
-	(d->owner == NULL) ? printf("Owner:(nil)")
+	(d->owner == NULL) ? printf("Owner: (nil)")
 		: printf("Owner: %s\n", d->owner);
 
 
