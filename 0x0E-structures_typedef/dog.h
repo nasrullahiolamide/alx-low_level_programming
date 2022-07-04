1dad6e6f11a369666fd332564716f71854c700dd
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-04 22:55:26 +0100

    Puppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Detail of a Dog
+ * @name: Pointer to the char array of name
+ * @age: Age of the dog
+ * @owner: Pointer to the char array of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
