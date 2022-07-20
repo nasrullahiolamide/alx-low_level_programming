cbfe246d91bd5f31af4b6ac52b38d40321ddf7e0
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-20 23:00:14 +0100

    insert node at middle

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to head pointer of linked list
+ * @idx: index to insert new node
+ * @n: new node's data
+ * Return: address of new node, or NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *prev;
+	listint_t *new_node;
+
+	prev = get_nodeint_at_index(*head, idx);
+	new_node = malloc(sizeof(listint_t));
+
+	new_node->n = n;
+	new_node->next = prev->next;
+	prev->next = new_node;
+
+	return (new_node);
+}
+
+/**
+ * get_nodeint_at_index - returns nth node of listint_t linked list
+ * @head: pointer to head pointer of linked list
+ * @index: nth node
+ * Return: nth node, or NULL if failed
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head != NULL && i < index)
+	{
+		i++;
+		head = head->next;
+	}
+	return (head);
+}
