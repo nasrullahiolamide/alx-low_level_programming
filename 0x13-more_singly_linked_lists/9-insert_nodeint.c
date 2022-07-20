e51be5dcfd5aec4e62ccc265e849064a4f17330b
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-20 23:10:53 +0100

    adjust logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	if (head == NULL)
 	{
