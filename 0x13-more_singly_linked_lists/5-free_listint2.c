c787a5a63eead8eb0bb10c48ca1a0230485b0924
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-21 01:21:56 +0100

    betty fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,19 @@
  */
 
 void free_listint2(listint_t **head)
-{	
+{
 	while (*head != NULL)
 	{
 		listint_t *tmp;
+
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
 	*head = NULL;
+
+	if (*head == NULL)
+	{
+		*head == NULL;
+	}
 }
