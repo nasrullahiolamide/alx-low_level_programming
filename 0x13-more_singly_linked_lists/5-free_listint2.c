316ed1cbb74c8069c1d1df80743f2da9e4d718bd
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-21 01:40:52 +0100

    one unsolved

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
+	
+	if (*head == NULL)
+		return;
+		
 	while (*head != NULL)
 	{
 		listint_t *tmp;
-
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
 	*head = NULL;
-
-	if (*head == NULL)
-	{
-		*head == NULL;
-	}
 }
