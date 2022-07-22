672b52ea743ac83cc1899d9ae3e4436174e4feed
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-22 19:52:42 +0100

    decimal to binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,15 +5,10 @@
 
 int _putchar(char c);
 
-/**
- * binary_to_unit - converts binary number to unsigned int.
- *
- * @b: binary to convert
- * Return: Unsigned int if successful and 0 if failed
- */
-unsigned int binary_to_uint(const char *b);
 
+unsigned int binary_to_uint(const char *b);
 unsigned int _strlen(const char *s);
+void print_binary(unsigned long int n);
 
 
 
