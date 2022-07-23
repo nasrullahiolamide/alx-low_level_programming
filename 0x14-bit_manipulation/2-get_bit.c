e5cc00526f6fab304a790725ea497c09f82ef184
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-23 07:07:50 +0100

    get bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,6 +17,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > max_bits)
 		return (-1);
 
+	
+	/* left shift by number of index and bitwise AND to get last bit */
 	ind_bit = ((n >> index) & 1);
 
 	return (ind_bit);
