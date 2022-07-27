117c7c5a1c4398c0de02f208f7493e551e82193c
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-27 12:41:58 +0100

    read file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-
+	int n_wrote;
 
 
 	if (filename == NULL)
@@ -27,7 +27,29 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
 
 	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,6 +17,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 
 
 #endif /* MAIN_H */
