de4b85e7729de5a3238f648f7e9289a485f5994a
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-07-27 12:39:12 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+
+	close(fd);
+	return (1);
+}
