5e22c4607925b63091f55c7d7dbe89ad7dda34bb
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-17 09:57:13 +0100

    fixed: Betty Style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint - Adds new node at the beginnig of the list
  *
- * @*head: pointer to the first node
+ * @head: pointer to the first node
  * @n: Value to be inputed
  * Return: Pointer to the new node.
  */
@@ -11,29 +11,30 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
-    dlistint_t *fmr = *(head);
+	dlistint_t *new;
+	dlistint_t *fmr = *(head);
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (NULL);
-    }
-    *head = new;
-    new->n = n;
-    new->prev = NULL;
-    if (fmr == NULL)
-    {
-        new->next = NULL;
-    }
-    else{
-        new->next = fmr;
-        fmr->prev = *(head);
-        fmr = NULL;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
+	*head = new;
+	new->n = n;
+	new->prev = NULL;
+	if (fmr == NULL)
+	{
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = fmr;
+		fmr->prev = *(head);
+		fmr = NULL;
 
-    }
+	}
 
-    return(new);
+	return	(new);
 
 }
